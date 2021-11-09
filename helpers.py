974bee0e2d51797cf19055ce970a0a5fdc10b080
metaTinker <michael.roust@gmail.com>
2021-11-09 09:21:12 +0100

    m

--- helpers.py
+++ helpers.py
@@ -13,3 +13,7 @@ QUOTES_2015_PATH = QUOTEBANK_FOLDER + 'quotes-2015.json.bz2'
 WIKIDATA_FOLDER = DATA_FOLDER + 'wikidata/'
 
 SPEAKER_ATTRIBUTES_PATH = WIKIDATA_FOLDER + 'speaker_attributes.parquet/'
+
+# Folder is meant to contain small snapshots of data files
+DATA_MINI_FOLDER = 'data_mini/'
+QUOTES_2020_PARTY_LABELED_SMALL_PATH = DATA_MINI_FOLDER + 'quotes-2020-party_labeled_small.json.bz2'
