bcf63cbad958ac0290025573130154ecfb1568f1
metaTinker <michael.roust@gmail.com>
2021-11-12 18:21:06 +0100

    swapping out gitignored data files with not ignored files

--- helpers.py
+++ helpers.py
@@ -23,11 +23,13 @@ SPEAKER_ATTRIBUTES_PATH = WIKIDATA_FOLDER + 'speaker_attributes.parquet/'
 
 # Folder is meant to contain small snapshots of data files
 DATA_MINI_FOLDER = 'data_mini/'
-QUOTES_2020_PARTY_LABELED_MINI_PATH = DATA_MINI_FOLDER + 'quotes-2020-party_labeled_mini.json'
-QUOTES_2020_PARTY_LABELED_SMALL_PATH = DATA_MINI_FOLDER + 'quotes-2020-party_labeled_small.json.bz2'
+# QUOTES_2020_PARTY_LABELED_MINI_PATH = DATA_MINI_FOLDER + 'quotes-2020-party_labeled_mini.json'
+# QUOTES_2020_PARTY_LABELED_SMALL_PATH = DATA_MINI_FOLDER + 'quotes-2020-party_labeled_small.json.bz2'
 
 #=============================================================================
 # Preprocessed files
+
+DATA_FOLDER = DATA_MINI_FOLDER
 
 PREPROCESSED_FOLDER = DATA_FOLDER + 'processed/'
 
